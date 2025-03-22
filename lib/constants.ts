bcf1e30208cceef4adf62ff337ecf9f174602c0b
peterk8213 <peterwilliams8213@gmail.com>
2025-03-22 22:43:58 +0300

    Update share link in constants to point to the new Worldcoin mini-app URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,7 @@
 export const shareData = {
   message:
     "I just sent money using this fast and secure transfer app! Try it out.",
-  link: process.env.APP_URL ?? "https://example.com",
+  link:
+    process.env.APP_URL ??
+    "https://worldcoin.org/mini-app?app_id=app_d826abbcef7ac8a14db406b6d2f7562d",
 };
